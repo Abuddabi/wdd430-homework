e8353d27a0bf6528d0ebb67fae62e388b0403970
Egor Sotnikov <web.abudabi@gmail.com>
2025-07-16 16:31:40 -0600

    Add form error handling

---
+++ app/ui/form-error.tsx
@@ -0,0 +1,13 @@
+export default function FormError({
+  error,
+  className,
+}: {
+  error: string;
+  className?: string;
+}) {
+  return (
+    <p className={`mt-2 text-sm text-red-500 ${className}`} key={error}>
+      {error}
+    </p>
+  );
+}
