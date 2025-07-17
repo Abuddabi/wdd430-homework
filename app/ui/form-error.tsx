e1c716f5f3edccac8ec10f4745d0246dba0ef4b2
Egor Sotnikov <web.abudabi@gmail.com>
2025-07-16 21:42:14 -0600

    Add authentication

--- app/ui/form-error.tsx
+++ app/ui/form-error.tsx
@@ -5,9 +5,5 @@ export default function FormError({
   error: string;
   className?: string;
 }) {
-  return (
-    <p className={`mt-2 text-sm text-red-500 ${className}`} key={error}>
-      {error}
-    </p>
-  );
+  return <p className={`mt-2 text-sm text-red-500 ${className}`}>{error}</p>;
 }
