b6c7baaf9385ee899095f8ecaf633335d22d778e
Egor Sotnikov <web.abudabi@gmail.com>
2025-07-16 22:00:17 -0600

    Add metadata

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,3 +1,9 @@
+import { type Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Login",
+};
+
 import AcmeLogo from "@/app/ui/acme-logo";
 import LoginForm from "@/app/ui/login-form";
 import { Suspense } from "react";
